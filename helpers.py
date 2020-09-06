3362e5d7aa7a35a10845fd1623c477dabfe4c2f5
Raúl Ortega Ochoa <69815654+raulorteg@users.noreply.github.com>
2020-09-06 19:21:47 +0200

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  6 16:49:41 2020
+
+@author: Raul Ortega
+"""
+
+def load_pattern(grid):
+    
+    # pattern given by the coordinates of the black grids
+    pattern = [[15, 15], [15, 14], [15, 16], [16, 15]]
+    
+    # initialize the grid array full of zeros, when at position of 
+    # pattern turn grid sqaure black, set 1
+    for pattern_dot in pattern:
+        # initialize the grid array full of zeros
+        grid[pattern_dot[0]][pattern_dot[1]] = 1
+    return grid
+
+# ===========================
+# function that given the grid computes the number of neighbors alive for each
+# node of the grid
+def alive_neighbors(grid):
+    m = len(grid)
+    n = len(grid[0])
+    temp = grid
+    
+    for row in range(1, m-1): # exclude edges for now
+            for column in range(1, n-1): # exclude edges for now
+                alive = 0 # alive neighbors counter
+                
+                # loop over its 8 neighbours
+                for i in [row-1, row, row+1]:
+                    for j in [column-1, column, column+1]:
+                        
+                        # if alive and not itself count as alive
+                        if (grid[i][j] == 1) and ([i, j] != [row, column]):
+                            alive = alive+1
+                            
+                        # rules function decides whether it lives or dies
+                temp[row][column] = rules(grid[row][column], alive)
+    return temp
+# ============================
+# function that given a number of alive neighbors decides weather the cell
+# lives or dies
+def rules(state, alive):
+    if state == 1: # cell was alive
+        if alive in [2, 3]:
+            return 1 # lives
+        else:
+            return 0 # dies
+    else: # cell was dead
+        if alive in [2, 3]:
+            return 1 # lives
+        else:
+            return 0 # dies
+                        
+                        
+                
+            
+    
--- main.py
+++ main.py
@@ -5,25 +5,30 @@ Created on Sun Sep  6 15:19:49 2020
 @author: Raul Ortega
 """
 import pygame
+from helpers import load_pattern, rules, alive_neighbors
+from time import sleep
 
 # define the two colors of the grid RGB
 black = (0, 0, 0)
 white = (255, 255, 255)
 
 # set the height/width of each location on the grid
-height = 20
+height = 5
 width = height # i want the grid square
-margin = 2 # sets margin between grid locations
+margin = 1 # sets margin between grid locations
 
 
 # initialize the grid array full of zeros
 grid = []
-num_rows = 20
+num_rows = 100
 num_columns = num_rows # i want it squared
 for row in range(num_rows):
     grid.append([])
     for column in range(num_columns):
         grid[row].append(0)
+
+# load the pattern that was pre-written in helpers.py
+load_pattern(grid)
 
 # initialize pygame
 pygame.init()
@@ -45,18 +50,18 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            # Change to grid coordinates
-            column = pos[0] // (width + margin)
-            row = pos[1] // (height + margin)
-            # Set that location to one
-            if grid[row][column] == 1:
-                grid[row][column] = 0
-            else:
-                grid[row][column] = 1
-            
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     # User clicks the mouse. Get the position
+        #     pos = pygame.mouse.get_pos()
+        #     # Change to grid coordinates
+        #     column = pos[0] // (width + margin)
+        #     row = pos[1] // (height + margin)
+        #     # Set that location to one
+        #     if grid[row][column] == 1:
+        #         grid[row][column] = 0
+        #     else:
+        #         grid[row][column] = 1
+    
     
     screen.fill(black) # fill background in black
     
@@ -67,18 +72,23 @@ while not done:
                 color = black
             else:
                 color = white
-            pygame.draw.rect(screen,
-                             color,
-                             [(margin + width) * column + margin,
-                              (margin + height) * row + margin,
-                              width,
-                              height])
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(margin + width) * column + margin,
+                                  (margin + height) * row + margin,
+                                  width,
+                                  height])
+    
     
     # set limit to 60 frames per second
     clock.tick(60)
     
     # update screen
     pygame.display.flip()
+    
+    grid = alive_neighbors(grid)
+    sleep(0.01)
+    
     
 pygame.quit() # so that it doesnt "hang" on exit
 
